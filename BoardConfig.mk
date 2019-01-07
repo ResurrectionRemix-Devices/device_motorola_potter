db83e7acf6fbf1d92ba46a7a9cfadfad8913291a
Vachounet <vachounet@live.fr>
2019-01-07 13:28:28 +0100

    potter: enforcing
    Change-Id: Ie9148a2d0829debeccbe5cc16d6e3b8d202e4f2d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_potter
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 2048
@@ -249,6 +249,10 @@ TARGET_USES_MKE2FS := true
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2018-09-01
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # VNDK
 PRODUCT_USE_VNDK_OVERRIDE := false
