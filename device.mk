31d6316f466891369e3f8cd2e4ab7fa1f5ffbfbe
joelh <joel.hans@outlook.com>
2019-01-23 10:29:04 -0500

    potter: Build Snap
    * Also alphabetize Camera packages
    
    Change-Id: Ie2e9d3a2f5d80ad784a6b45c9a7d5609c9b4354d

--- device.mk
+++ device.mk
@@ -101,14 +101,14 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libbson \
-    Camera2 \
+    android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
+    libbson \
+    libjustshoot_shim \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    libjustshoot_shim
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # CarrierConfig
 PRODUCT_PACKAGES += \
